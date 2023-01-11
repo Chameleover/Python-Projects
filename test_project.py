052dc963ceef457b11453afb4bf85af8bbb4c6ce
Chameleover <wargan506@gmail.com>
2023-01-11 15:27:25 +0200

    article added in a new dict file formatted, code cleared up
    TODO:
    convert that dict to a good looking email
    --
    add more websties
    add more keywords

--- test_project.py
+++ test_project.py
@@ -4,6 +4,7 @@ import re
 from newspaper import Article
 import csv
 
+
 def main():
     kaldata_beauty = get_kaldata_articles()
     kaldata_links = convert_beauty_to_links(kaldata_beauty)
@@ -11,25 +12,23 @@ def main():
     kaldata_dict = {}
     for i, articles in enumerate(kaldata_links):
         kaldata_dict[kaldata_beauty[i].get_text()] = kaldata_links[i]
-    #print(kaldata_dict.values())
+    # print(kaldata_dict.values())
 
     print(check_for_keywords(kaldata_dict))
+
 
 class Newspaper:
     '''Class for each newspaper included in the web scraper
 
     Returns parsed html page by passing it to the BeatufulSoup constructor. 'html.parser' uses the best available
     parseer. Might be slow if website has way too many articles.
-
-
     '''
+
     def __init__(self, url):
         self.url = url
 
-
     def get_content(self):
         return requests.get(self.url)
-
 
     def parse_articles(self):
         articles = self.get_content().content
@@ -37,67 +36,39 @@ class Newspaper:
         return beauty
 
 
-
-# class NewsArticle:
-#     '''Class for each article
-#
-#     Processes the article according to the newspaper. Extracts articles, which includes required keywords
-#
-#     '''
-#
-#     def __init__(self, url):
-#         self.url = url
-#
-#
-#
-#     def extract_content(self):
-#         content = requests.get(self.url).text
-#         parsed_content = BeautifulSoup(content, 'html.parser')
-#
-#         return parsed_content
-
 def check_for_keywords(article_dict):
     '''
     Checks given article dictionary values (links) if they contain given keywords are passed in from file
+    and store title:link in another dictionary
     :param article_dict:
     :return:
     '''
+    # Create an empty dict to store accepted articles
     dict_of_article_content = {}
 
-    for value in article_dict.values():
-
-        base_url = value
-
-        article = Article(base_url)
+    # Iterate over all articles 1 by 1
+    for article_url in article_dict.values():
 
         # Extract only article content from the web page
+        article = Article(article_url)
         article.download()
         article.parse()
         article_to_list = list(article.text.split(' '))
         keywords = get_keywords()
 
+        # Store each article, containing a keyword in a new dict
         if any(word in keywords for word in article_to_list):
-            print(f"keyword found in article {article.title}")
-        #return article.text
+            print(f"Article: \"{article.title}\" stored")
+            dict_of_article_content[article.title] = article_url
 
+    return dict_of_article_content
 
-    #return article_content.text
-
-
-    # r = requests.get(base_url)
-    # if "kaldata" in base_url:
-    #     soup = BeautifulSoup() # find('div', class_='td-post-content tagdiv-type')
-    # soup = BeautifulSoup(r.text, 'html.parser')
-    # return soup.text
 
 def get_kaldata_articles():
-
     kaldata_url = "https://www.kaldata.com/"
-
     kaldata = Newspaper(kaldata_url)
-
-    # kaldata article parameters: 'h3', class_='entry-title td-module-title'
     kaldata_news = kaldata.parse_articles().find_all('h3', class_='entry-title td-module-title')
+
     return kaldata_news
 
 
